7da2b6452da98d9d13f6a8866268367b1303b438
Lucas Wiener <lucas@wiener.se>
2014-10-24 16:24:56 +0200

    First versio only turning left

---
+++ src/explorer_node.cpp
@@ -0,0 +1,51 @@
+#include <ros/ros.h>
+#include <actionlib/client/simple_action_client.h>
+#include <s8_common_node/Node.h>
+
+#include <s8_turner/TurnAction.h>
+
+
+#define NODE_NAME		"s8_explorer_node"
+
+#define ACTION_TURN		"/s8_turner/turn"
+
+class Explorer : public s8::Node {
+	actionlib::SimpleActionClient<s8_turner::TurnAction> turn_action;
+
+
+public:
+	Explorer() : turn_action(ACTION_TURN, true) {
+        ROS_INFO("Waiting for turn action server...");
+        turn_action.waitForServer();
+        ROS_INFO("Connected to turn action server!");
+
+        turn(90);
+	}
+
+	void turn(int degrees) {
+		s8_turner::TurnGoal goal;
+		goal.degrees = degrees;
+		turn_action.sendGoal(goal);
+
+
+        bool finised_before_timeout = turn_action.waitForResult(ros::Duration(30.0));
+
+        if(finised_before_timeout) {
+            actionlib::SimpleClientGoalState state = turn_action.getState();
+            if(state == actionlib::SimpleClientGoalState::SUCCEEDED) {
+            	ROS_INFO("Turn action succeeded. Degrees turn: %d", turn_action.getResult()->degrees);
+            } else {
+            	ROS_INFO("Turn action finished with unknown state: %s", state.toString().c_str());
+            }
+        } else {
+            ROS_WARN("Turn action timed out.");
+        }
+	}
+};
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, NODE_NAME);
+	Explorer explorer;
+	ros::spin();
+	return 0;
+}
